aebc68e1de14ffeea13f88da3682ec613ad31184
santiyoo <yoopy2705@gmail.com>
2022-05-13 18:48:45 -0300

    reactv1

---
+++ movies-react/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+    return ( 
+        <div className="container text-center p-4">
+            <div className="row">
+                <div className="col-md-12">
+                    &copy; 2021 - Movies - Abraldes & Yoo
+                </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default Footer;
---
+++ movies-react/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+const Header = () => {
+    return ( 
+        <div className="col-md-8 align-end">
+            <Navbar />                    
+        </div>
+    );
+}
+ 
+export default Header;
