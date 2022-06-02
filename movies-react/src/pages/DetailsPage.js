18d7ea4e7007fbfc3d95e0afd4a3693681659570
joacoabraldes <abraldesjoaquin@gmail.com>
2022-06-02 10:34:43 -0300

    arreglar director

--- movies-react/src/pages/DetailsPage.js
+++ movies-react/src/pages/DetailsPage.js
@@ -5,8 +5,9 @@ import { peliXId, credits } from "../Fetch";
 import { useState, useEffect } from "react";
 
 export default function DetailsPage() {
+
   const {id}=useParams(); 
-  const [pelicula, setPelicula] = useState([]);
+  const [pelicula, setPelicula] = useState({});
   const [creditos, setCreditos] = useState(undefined)
 
     useEffect(() => {
@@ -21,46 +22,49 @@ export default function DetailsPage() {
     
     useEffect(()=>{
     }, [creditos])
-    
-    return creditos&& (
+
+    const myStyle={
+      backgroundImage: `url('https://image.tmdb.org/t/p/w200/${pelicula.backdrop_path}')`,
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',  
+      opacity: '50%'
+    };
+    return pelicula && creditos ? (
     <>
-    {/* <div>
-      User <h2>{id}</h2>
-    </div> */}
-
-
 
     <div className="container mt-3">
       <div className="row">
+      {/* <div className="row"> */}
           <div className="col-md-4">
-              <img src={`https://image.tmdb.org/t/p/w200/${pelicula.poster_path}`} alt="" width="85%"/>
+              <img src={`https://image.tmdb.org/t/p/w200/${pelicula.poster_path}`} alt="" width="100%"/>
           </div>
           <div className="col-md-8">
-            <h2><b>{ pelicula.title }</b> ({ pelicula.release_date })</h2>
-            <p>PG-13 11/11/2012 - Ciencia Ficción</p>
+            <h2><b>{ pelicula.title }</b></h2>
+            <p><b>({ pelicula.release_date })</b> - { pelicula.genres?.map(genre => (genre.name + " - ")) }</p>
             <h4>Resumen</h4>
             <p>{pelicula.overview}</p>
+ 
             <div className="row">
               <div className="col-md-4">
-                  <h6>Director</h6>
-                  <p>{creditos.crew.original_name}</p>
-                  <h6>Director</h6>
-                  <p>Nombre Apellido</p>
+                {creditos.crew.map((x)=>{
+                  if (x.job === "Director") {
+                    return(
+                      <div className='ml-5'>
+                        <img src={`https://image.tmdb.org/t/p/w200/${x.profile_path}`} alt="" width="40%"/>
+                        <h6>{x.job}</h6>
+                        <p>{ creditos.crew.filter(crew => crew.job === "Director")[0].name }</p>
+                      </div>
+                  )
+                }
+                })}
               </div>
-              <div className="col-md-4">
+              {/* <div className="col-md-4">
                   <h6>Director</h6>
                   <p>Nombre Apellido</p>
               
                   <h6>Director</h6>
                   <p>Nombre Apellido</p>
-              </div>
-              <div className="col-md-4">
-                  <h6>Director</h6>
-                  <p>Nombre Apellido</p>
-              
-                  <h6>Director</h6>
-                  <p>Nombre Apellido</p>
-              </div>
+              </div> */}
             </div>
           </div>
       </div>
@@ -86,19 +90,20 @@ export default function DetailsPage() {
       </div>
       <div className="col-md-2">
         <p><b>Status</b></p>
-        <p>Released</p>
+        <p>{pelicula.status}</p>
         <br/>
-        <p><b>Status</b></p>
-        <p>Released</p>
+        <p><b>Orginal Language</b></p>
+        <p>{ pelicula.spoken_languages?.map(spoken_languages => (spoken_languages.name)) }</p>
         <br/>
-        <p><b>Status</b></p>
-        <p>Released</p>
+        <p><b>Budget</b></p>
+        <p>${pelicula.budget}</p>
         <br/>
-        <p><b>Status</b></p>
-        <p>Released</p>
+        <p><b>Revenue</b></p>
+        <p>${pelicula.revenue}</p>
       </div>
     </div>
     </div>
     </>
-  );
+  )
+  : <>Cargando</>;
 }
